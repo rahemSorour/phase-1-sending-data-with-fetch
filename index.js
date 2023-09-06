5cd4ff5bcc9615a4de249491b24c18cfd8e2ec1c
rahem sorour <sorourrahem17@gmail.com>
2023-09-06 21:51:32 +0100

    completed assignemet

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+   
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+          })
+      })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(data=> {
+        console.log(data)
+        document.body.innerHTML = data.id;
+      })
+      .catch(error=> {
+        document.body.innerHTML = error.message;
+      });
+  }
+  
+//   submitData("Steve", "steve@steve.com")
